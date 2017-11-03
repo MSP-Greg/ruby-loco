c6cf9ec1f4fed7d97f630fe6ef7459d09c8ea785
MSP-Greg <MSP-Greg@users.noreply.github.com>
2017-11-02 22:35:24 -0500

    Fix skips shown [skip ci]

--- test_script.rb
+++ test_script.rb
@@ -18,7 +18,7 @@ if log.length == 1
       failures += results[/assertions, (\d+) failures?/,1].to_i + results[/failures?, (\d+) errors?/,1].to_i
       # find last skipped
       skips_shown = 0
-      s.scan(/^ +(\d+)\) Skipped:/) { |m| skips_shown += 1 }
+      s.scan(/^ *(\d+)\) Skipped:/) { |m| skips_shown += 1 }
       results << ", #{skips_shown} skips shown"
     else
       failures += 1
