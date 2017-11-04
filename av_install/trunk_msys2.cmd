7be45a1a29993d120520812a7b1bfb4aab3abdc9
MSP-Greg <MSP-Greg@users.noreply.github.com>
2017-11-03 21:13:57 -0500

    Update to 1.1.0g & BinTray uri [skip ci]

--- av_install/trunk_msys2.cmd
+++ av_install/trunk_msys2.cmd
@@ -7,7 +7,8 @@
 @pacman -Sy --noconfirm --needed mingw-w64-x86_64-toolchain mingw-w64-x86_64-gcc-libs mingw-w64-x86_64-gmp mingw-w64-x86_64-libffi mingw-w64-x86_64-zlib
 
 @set    gdbm=mingw-w64-x86_64-gdbm-1.10-2-any.pkg.tar.xz
-@set openssl=mingw-w64-x86_64-openssl-1.1.0.f-1-any.pkg.tar.xz
+@set openssl=mingw-w64-x86_64-openssl-1.1.0.g-1-any.pkg.tar.xz
+@set  dl_uri=https://dl.bintray.com/msp-greg/ruby_trunk
 
 @echo ——————————————————————————————————————————————————————————————— Adding GPG key
 @bash -lc "pacman-key -r 77D8FA18 --keyserver na.pool.sks-keyservers.net && pacman-key -f 77D8FA18 && pacman-key --lsign-key 77D8FA18"
@@ -15,14 +16,14 @@
 @md C:\pkgs
 
 @echo ——————————————————————————————————————————————————————————————— Installing gdbm-1.10
-appveyor DownloadFile https://dl.bintray.com/msp-greg/ruby_windows/%gdbm%     -FileName C:\pkgs\%gdbm%
-appveyor DownloadFile https://dl.bintray.com/msp-greg/ruby_windows/%gdbm%.sig -FileName C:\pkgs\%gdbm%.sig
+appveyor DownloadFile %dl_uri%/%gdbm%     -FileName C:\pkgs\%gdbm%
+appveyor DownloadFile %dl_uri%/%gdbm%.sig -FileName C:\pkgs\%gdbm%.sig
 @pacman -Rdd --noconfirm mingw-w64-x86_64-gdbm > nul
 @pacman -Udd --noconfirm --force  /c/pkgs/%gdbm%    > nul
 
 @echo ——————————————————————————————————————————————————————————————— Installing openssl-1.1.0
-appveyor DownloadFile https://dl.bintray.com/msp-greg/ruby_windows/%openssl%     -FileName C:\pkgs\%openssl%
-appveyor DownloadFile https://dl.bintray.com/msp-greg/ruby_windows/%openssl%.sig -FileName C:\pkgs\%openssl%.sig
+appveyor DownloadFile %dl_uri%/%openssl%     -FileName C:\pkgs\%openssl%
+appveyor DownloadFile %dl_uri%/%openssl%.sig -FileName C:\pkgs\%openssl%.sig
 @pacman -Rdd --noconfirm mingw-w64-x86_64-openssl > nul
 @pacman -Udd --noconfirm --force /c/pkgs/%openssl%     > nul
 
