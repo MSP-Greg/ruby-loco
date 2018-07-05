777753d87a072bf8c8cdbfe7cd752cf6351cc32f
MSP-Greg <MSP-Greg@users.noreply.github.com>
2018-07-05 15:10:53 -0500

    Remove RUBY_FORCE_TEST_JIT

--- pkg_build_64.cmd
+++ pkg_build_64.cmd
@@ -93,7 +93,7 @@ bash.exe --login -c  "cd '%DP0%'; MINGW_INSTALLS=mingw64 makepkg-mingw --nocheck
 @echo test-all
 
 @if "%R_VERS_2%" GEQ "25" (
-  set RUBY_FORCE_TEST_JIT=1
+@rem 2018-07-06 set RUBY_FORCE_TEST_JIT=1
   make test-all "TESTOPTS=-a -j%M_JOBS% --job-status=normal --show-skip --retry --subprocess-timeout-scale=1.5" > %LOG_PATH_NAME%-test-all.log 2>&1
 @rem  timeout.exe 25m make test-all "TESTOPTS=--verbose -j%M_JOBS% --job-status=normal --show-skip --retry --subprocess-timeout-scale=1.5"
 ) else (
