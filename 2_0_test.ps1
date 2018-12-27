612472015d99daddbcc06a9514516121e50a5241
MSP-Greg <greg.mpls@gmail.com>
2018-12-26 21:51:14 -0600

    Add tzinfo-data

--- 2_0_test.ps1
+++ 2_0_test.ps1
@@ -215,7 +215,7 @@ $ruby_exe  = "$d_install/bin/ruby.exe"
 $abi       = &$ruby_exe -e "print RbConfig::CONFIG['ruby_version']"
 $script:time_info = ''
 
-&$d_install/bin/gem install `"timezone:>=1.3.2`" `"tzinfo:>=1.2.5`" --no-document
+&$d_install/bin/gem install `"timezone:>=1.3.2`" `"tzinfo:>=2.0.0`" `"tzinfo-data:>=1.2018.7`" --no-document
 
 #————————————————————————————————————————————————————————————————— start testing
 # test using readline.so, not rb-readline
