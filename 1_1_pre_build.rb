eaa88cff8fbbed226881b508b003f774963d051d
MSP-Greg <greg.mpls@gmail.com>
2018-12-27 22:37:50 -0600

    update for r66602 version info changes

--- 1_1_pre_build.rb
+++ 1_1_pre_build.rb
@@ -38,25 +38,35 @@ class << self
       end
 
       # open version.h and get ruby info
+      version, title = nil, nil
+
       File.open('version.h', 'rb:utf-8') { |f|
         v_data = f.read
         version = v_data[/^#define[ \t]+RUBY_VERSION[ \t]+["']([\d\.]+)["']/, 1]
         patch   = v_data[/^#define[ \t]+RUBY_PATCHLEVEL[ \t]+(-?\d+)/, 1]
-
         date    = v_data[/^#define[ \t]+RUBY_RELEASE_YEAR[ \t]+(\d{4})/, 1] + '-' +
                   v_data[/^#define[ \t]+RUBY_RELEASE_MONTH[ \t]+(\d{1,2})/, 1].rjust(2,'0') + '-' +
                   v_data[/^#define[ \t]+RUBY_RELEASE_DAY[ \t]+(\d{1,2})/, 1].rjust(2,'0')
         patch = patch == '-1' ? 'dev' : "p#{patch}"
-        vers_abi = version.sub(/\d+$/, '0')
-
-        # Add title to Appveyor build
         arch = ARCH == '64' ? '[x64-mingw32]' : '[i386-mingw32]'
-        title = "ruby #{version}#{patch} (#{date} #{branch} #{svn}) #{arch}"
-        if ENV['APPVEYOR']
-          `appveyor UpdateBuild -Message \"#{title}\"`
-        end
-        puts title
+        title = "#{patch} (#{date} #{branch} #{svn}) #{arch}"
       }
+      # needed for r66602 and later
+      if version.nil?
+        File.open('include/ruby/version.h', 'rb:utf-8') { |f|
+          v_data = f.read
+          version = v_data[/^#define RUBY_API_VERSION_MAJOR (\d+)/, 1] + '.' +
+                    v_data[/^#define RUBY_API_VERSION_MINOR (\d+)/, 1] + '.' +
+                    v_data[/^#define RUBY_API_VERSION_TEENY (\d+)/, 1]
+        }
+      end
+      title = "ruby #{version}" + title
+
+      # Add title to Appveyor build
+      if ENV['APPVEYOR']
+        `appveyor UpdateBuild -Message \"#{title}\"`
+      end
+      puts title
     }
   end
 
