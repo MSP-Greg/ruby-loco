e8ddfdda7b8b0498576cb02dd5ae520815ba16cd
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-05-28 15:01:27 -0500

    trunk_msys2.ps1 - add pdcurses, toolchain --nodeps, etc

--- trunk_msys2.ps1
+++ trunk_msys2.ps1
@@ -80,13 +80,15 @@ pacman.exe -S --noconfirm --needed --noprogressbar base 2> $null
 #>
 
 Write-Host "$($dash * 63) Updating MSYS2 / MinGW toolchain" -ForegroundColor $fc
-pacman.exe -Sy --noconfirm --needed --noprogressbar $($pre + 'toolchain') 2> $null
+pacman.exe -Sy --noconfirm --needed --noprogressbar --nodeps $($pre + 'toolchain') 2> $null
 Check-Exit 'Cannot update toolchain'
 
 Write-Host "$($dash * 63) Updating MSYS2 / MinGW ruby depends" -ForegroundColor Yellow
-$tools =  "___gdbm ___gmp ___libffi ___openssl ___ragel ___readline ___termcap ___zlib".replace('___', $pre)
-#pacman.exe -S --noconfirm --needed --noprogressbar $tools.split(' ') 2> $null
+$tools = "___python3 ___readline ___sqlite3".replace('___', $pre)
 pacman.exe -S --noconfirm --needed --noprogressbar $tools.split(' ')
+
+$tools = "___gdbm ___gmp ___libffi ___openssl ___pdcurses ___ragel ___zlib".replace('___', $pre)
+pacman.exe -S --noconfirm --needed --noprogressbar $tools.split(' ') 2> $null
 
 # As of Sept-2018 libyaml is not installed on Appveyor
 # pacman -Rdd --noconfirm mingw-w64-x86_64-libyaml
