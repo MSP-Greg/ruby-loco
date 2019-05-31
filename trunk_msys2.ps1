f6874c9e90e300f94bb190c08b04e839c3031892
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-05-31 11:13:38 -0500

    Update trunk_msys2.ps1

--- trunk_msys2.ps1
+++ trunk_msys2.ps1
@@ -81,10 +81,10 @@ pacman.exe -S --noconfirm --needed --noprogressbar base 2> $null
 
 Write-Host "$($dash * 63) Updating MSYS2 / MinGW ruby depends 1" -ForegroundColor Yellow
 $tools = "___python3 ___readline ___sqlite3".replace('___', $pre)
-pacman.exe -S --noconfirm --needed --noprogressbar $tools.split(' ')
+pacman.exe -Sy --noconfirm --needed --noprogressbar $tools.split(' ')
 
 Write-Host "$($dash * 63) Updating MSYS2 / MinGW toolchain" -ForegroundColor $fc
-pacman.exe -Sy --noconfirm --needed --noprogressbar --nodeps $($pre + 'toolchain') 2> $null
+pacman.exe -S --noconfirm --needed --noprogressbar --nodeps $($pre + 'toolchain') 2> $null
 Check-Exit 'Cannot update toolchain'
 
 Write-Host "$($dash * 63) Updating MSYS2 / MinGW ruby depends 2" -ForegroundColor Yellow
