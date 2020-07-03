0e0e97edf07de78ef213e75da3cf523ccfcd84fe
MSP-Greg <Greg.mpls@gmail.com>
2020-07-03 12:18:25 -0500

    Remove SPEC_TEMP_DIR

--- 1_3_post_install.rb
+++ 1_3_post_install.rb
@@ -171,7 +171,7 @@ class << self
     Dir.chdir BINDIR do
       bash_bins = Dir["*"].select { |fn| !Dir.exist?(fn) && File.extname(fn).empty? }
       bash_bins.each { |fn|
-        str = File.read(fn, mode: 'rb:UTF-8').sub(/^#![^\n]+ruby/, '#!/usr/bin/env ruby')
+        str = File.read(fn, mode: 'rb:UTF-8').sub(/^#![^\n]+ruby$/, '#!/usr/bin/env ruby')
         File.write fn, str, mode: 'wb:UTF-8'
       }
     end
