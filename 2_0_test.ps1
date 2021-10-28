15305a8039ecf643d535dfa6fdc2ae77ca659370
MSP-Greg <Greg.mpls@gmail.com>
2021-10-28 13:06:10 -0500

    test-all limit to 2,300 s

--- 2_0_test.ps1
+++ 2_0_test.ps1
@@ -221,7 +221,7 @@ function Test-All {
     -StdErr "test_all_err.log" `
     -Title  "test-all" `
     -Dir    "$d_ruby/test" `
-    -TimeLimit 2100
+    -TimeLimit 2300
 
   # comment out below to allow full testing of Appveyor artifact
   # Remove-Item -Path "$d_install/lib/ruby/$abi/$rarch/-test-" -Recurse
