57fe6b3f7fbe2e7058b036680c36112b6021d125
David Rodríguez <deivid.rodriguez@riseup.net>
2024-10-11 02:30:41 +0200

    Remove no longer necessary code to run Ruby from same folder (#15)

--- 1_4_post_install_bin_files.rb
+++ 1_4_post_install_bin_files.rb
@@ -13,15 +13,6 @@ module CopyBashScripts
       ary = Dir["#{Gem.dir}/gems/*"]
       ary.each { |d| Dir.rmdir(d) if Dir.empty?(d) }
 
-      bash_preamble = <<~BASH.strip.rstrip
-        #!
-        {
-          bindir=$(dirname "$0")
-          exec "$bindir/ruby" "-x" "$0" "$@"
-        }
-        #!/usr/bin/env ruby
-      BASH
-
       windows_script = <<~BAT
         @ECHO OFF
         @"%~dp0ruby.exe" -x "%~dpn0" %*
@@ -35,8 +26,8 @@ module CopyBashScripts
 
       # file permissions may not work on Windows, especially execute
       bash_bins.each do |fn|
-        ruby_code = File.read(fn, mode: 'rb:UTF-8').split(/^#![^\n]+ruby/,2).last.lstrip
-        File.write fn, "#{bash_preamble}\n#{ruby_code}", mode: 'wb:UTF-8'
+        str = File.read(fn, mode: 'rb:UTF-8').sub(/^#![^\n]+ruby/, '#!/usr/bin/env ruby')
+        File.write fn, str, mode: 'wb:UTF-8'
       end
 
       windows_bins = bins.select { |fn| File.extname(fn).match?(/\A\.bat|\A\.cmd/) }
@@ -45,8 +36,8 @@ module CopyBashScripts
         # 'gem' bash script doesn't exist
         bash_bin = "#{BIN_DIR}/#{File.basename fn, '.*'}"
         unless File.exist? bash_bin
-          ruby_code = File.read(fn, mode: 'rb:UTF-8').split(/^#![^\n]+ruby/,2).last.lstrip
-          File.write bash_bin, "#{bash_preamble}\n#{ruby_code}", mode: 'wb:UTF-8'
+          str = File.read(fn, mode: 'rb:UTF-8').sub(/^#![^\n]+ruby/, '#!/usr/bin/env ruby')
+          File.write bash_bin, str, mode: 'wb:UTF-8'
         end
 
         File.write fn, windows_script, mode: 'wb:UTF-8'
